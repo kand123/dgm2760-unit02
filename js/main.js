e1407e41b86122cad4263caa2f99a952bfb026ec
Kristin <kbrandtsojo@gmail.com>
2020-01-14 11:27:12 -0700

    added some styling

--- js/main.js
+++ js/main.js
@@ -2,12 +2,10 @@ document.querySelector('header > h1').innerText = "Inspector Clouseau"
 document.querySelector('header > h2').innerText = "asdf"
 
 
-
 function reDisplay(){
-let windowSizes = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall`
-let offset = `Window offset is ${window.screenX} from the left edge and ${window.screenY} pixels from the top`
+let windowSizes = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`
+let offset = `Window offset is ${window.screenX} from the left edge and ${window.screenY} pixels from the top.`
 let location =  `The URL for this site is ${window.location}.`
-
 
 
 
@@ -18,18 +16,11 @@ let location =  `The URL for this site is ${window.location}.`
 // document.body.appendChild(anc)
 
 
-
 let myWindow = document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset} \n ${location}`
-
-
-
-
 
 }
 
-
 reDisplay()
-
 
 
 let docTitle = `This document is titled ${document.title}.`
