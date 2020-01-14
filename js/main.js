501fc7c9a097c5fa92c775b072ad21c1103e6f03
Kristin <kbrandtsojo@gmail.com>
2020-01-13 19:37:32 -0700

    Added window properties with js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+document.querySelector('header > h1').innerText = "Inspector Clouseau"
+document.querySelector('header > h2').innerText = "asdf"
+
+
+
+function reDisplay(){
+let windowSizes = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall`
+let offset = `Window offset is ${window.screenX} from the left edge and ${window.screenY} pixels from the top`
+let location =  `The URL for this site is ${window.location}.`
+
+
+
+
+// let anc = document.createElement('a')
+// let link = document.createTextNode(location)
+// anc.appendChild(link)
+// anc.href = `${window.location}`
+// document.body.appendChild(anc)
+
+
+
+let myWindow = document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset} \n ${location}`
+
+}
+
+
+reDisplay()
+
+
