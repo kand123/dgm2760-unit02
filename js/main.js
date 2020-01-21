49df3f7fb5e7d6a47bf548b8303d08df8ebd501d
Kristin <kbrandtsojo@gmail.com>
2020-01-20 20:02:34 -0700

    updated font styling

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
 document.querySelector('header > h1').innerText = "Inspector Clouseau"
-document.querySelector('header > h2').innerText = "asdf"
+document.querySelector('header > h2').innerText = "#1 Mystery Solver in the Tri-state area"
 
 
 function reDisplay(){
 let windowSizes = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`
 let offset = `Window offset is ${window.screenX} from the left edge and ${window.screenY} pixels from the top.`
-let location =  `The URL for this site is ${window.location}.`
+let location =  `The URL for this site is ${window.location}`
 
 
-
-// let anc = document.createElement('a')
-// let link = document.createTextNode(location)
-// anc.appendChild(link)
-// anc.href = `${window.location}`
-// document.body.appendChild(anc)
 
 
 let myWindow = document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset} \n ${location}`
